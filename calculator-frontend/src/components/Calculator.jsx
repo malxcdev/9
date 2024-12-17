ff602479b49196801136f5e6a6cf23110466253b
malxcdev <malxcondev@gmail.com>
2024-12-16 19:44:35 -0500

    Estructurando HTML

---
+++ calculator-frontend/src/components/Calculator.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Calculator() {
+    return (
+        <section className='bg-white'>
+            <div>
+                <div>
+                    <h1>Mortgage Calculator</h1>
+                    <a href="#">Clear All</a>
+                </div>
+                <form>
+                    <div>
+                        <label>Mortgage Amount</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <div>
+                            <label>Mortgage Term</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label>Interest Rate</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <label>Mortgage Type</label>
+                        <label><input type="checkbox" name="" id="" />Repayment</label>
+                        <label><input type="checkbox" name="" id="" />Interest Only</label>
+                    </div>
+                    <button>Calculate Repayment</button>
+                </form>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>Result Shown Here</h2>
+                <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Calculator
